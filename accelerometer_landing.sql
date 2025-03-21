d2b157a53e2ecb427176b0415ad2273f7feed32f
TariqMun <tariq.munshi@gmail.com>
2025-03-21 15:01:57 +0000

    Update accelerometer_landing.sql

--- accelerometer_landing.sql
+++ accelerometer_landing.sql
@@ -1,1 +1,11 @@
-
+CREATE EXTERNAL TABLE 
+accelerometer_landing (
+  user, STRING,
+  timestamp, BIGINT,
+  x, FLOAT,
+  y, FLOAT,
+  z, FLOAT
+  )
+ROW FORMAT SERDE
+'org.openx.data.jsonserde.JspmSerDe'
+LOCATION 's3://stedi-human-balance-20250312/accelerometer-landing/
